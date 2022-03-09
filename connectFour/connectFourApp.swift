d332174c03b598402ebeca54e315e553ac138dd9
moonsword5083 <anthea880702@gmail.com>
2022-03-09 10:56:59 +0800

    Initial Commit

---
+++ connectFour/connectFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectFourApp.swift
+//  connectFour
+//
+//  Created by User23 on 2022/3/9.
+//
+
+import SwiftUI
+
+@main
+struct connectFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
